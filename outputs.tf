ff91f2c9337cff1fa8db3e88e516bf71f28de071
therahul28 <therahulyadav0077@gmail.com>
2023-04-04 13:07:38 +0530

    first commit

---
+++ outputs.tf
@@ -0,0 +1,26 @@
+# Module      : Iam Role
+# Description : Terraform module to create Iam Role resource on AWS.
+output "arn" {
+  value       = join("", aws_iam_role.default.*.arn)
+  description = "The Amazon Resource Name (ARN) specifying the role."
+}
+
+output "tags" {
+  value       = module.labels.tags
+  description = "A mapping of tags to assign to the resource."
+}
+
+output "name" {
+  value       = join("", aws_iam_role.default.*.name)
+  description = "Name of specifying the role."
+}
+
+output "policy" {
+  value       = join("", aws_iam_role_policy.default.*.policy)
+  description = "The policy document attached to the role."
+}
+
+output "role" {
+  value       = join("", aws_iam_role_policy.default.*.role)
+  description = "The name of the role associated with the policy."
+}
